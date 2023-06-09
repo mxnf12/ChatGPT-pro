31629d292618f79eaaf315901eff10f1e25da69e
mxnf12 <130280770+mxnf12@users.noreply.github.com>
2023-06-09 23:49:34 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,10 +1,10 @@
 export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=D6HaKWVOz8koo4aox3N4-pDWFnxLoOzm&authKey=zYZbx0%2BSGDxpuUiA58YMNMjqGhP12XWDfYhhvTvKPW9ri86kf7jxPdOmru6ccO29&noverify=0&group_code=467599493`;
-export const ISSUE_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=D6HaKWVOz8koo4aox3N4-pDWFnxLoOzm&authKey=zYZbx0%2BSGDxpuUiA58YMNMjqGhP12XWDfYhhvTvKPW9ri86kf7jxPdOmru6ccO29&noverify=0&group_code=467599493`;
+export const REPO_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=DyoNwC1wHiqQHeOxJLb2rnvo2MF4BzkJ&authKey=GmBN4K4dG4VSEiCqRoDWTKUywENGT3ozPHgAIukoqlZHJLfxugQDy%2FY1gd475Ncr&noverify=0&group_code=854495421`;
+export const ISSUE_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=DyoNwC1wHiqQHeOxJLb2rnvo2MF4BzkJ&authKey=GmBN4K4dG4VSEiCqRoDWTKUywENGT3ozPHgAIukoqlZHJLfxugQDy%2FY1gd475Ncr&noverify=0&group_code=854495421`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-export const FETCH_COMMIT_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=D6HaKWVOz8koo4aox3N4-pDWFnxLoOzm&authKey=zYZbx0%2BSGDxpuUiA58YMNMjqGhP12XWDfYhhvTvKPW9ri86kf7jxPdOmru6ccO29&noverify=0&group_code=467599493`;
-export const FETCH_TAG_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=D6HaKWVOz8koo4aox3N4-pDWFnxLoOzm&authKey=zYZbx0%2BSGDxpuUiA58YMNMjqGhP12XWDfYhhvTvKPW9ri86kf7jxPdOmru6ccO29&noverify=0&group_code=467599493`;
+export const FETCH_COMMIT_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=DyoNwC1wHiqQHeOxJLb2rnvo2MF4BzkJ&authKey=GmBN4K4dG4VSEiCqRoDWTKUywENGT3ozPHgAIukoqlZHJLfxugQDy%2FY1gd475Ncr&noverify=0&group_code=854495421`;
+export const FETCH_TAG_URL = `http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=DyoNwC1wHiqQHeOxJLb2rnvo2MF4BzkJ&authKey=GmBN4K4dG4VSEiCqRoDWTKUywENGT3ozPHgAIukoqlZHJLfxugQDy%2FY1gd475Ncr&noverify=0&group_code=854495421`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export enum Path {
